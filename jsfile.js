f3744139ea485a1d409fbc1ce161eac40dbf2e1d
wayskid <wayskid02@gmail.com>
2021-11-20 17:43:58 -0500

    First commit

---
+++ jsfile.js
@@ -0,0 +1,168 @@
+`use strict`;
+
+let inputDisplay = document.querySelector(".screen");
+
+let oneValue = document.querySelector(".btnOne").textContent;
+let btnOne = document.querySelector(".btnOne");
+
+let btnTwo = document.querySelector(".btnTwo");
+let twoValue = document.querySelector(".btnTwo").textContent;
+
+
+let btnThree = document.querySelector(".btnThree");
+let threeValue = document.querySelector(".btnThree").textContent;
+
+
+let btnFour = document.querySelector(".btnFour");
+let fourValue = document.querySelector(".btnFour").textContent;
+
+
+let btnFive = document.querySelector(".btnFive");
+let fiveValue = document.querySelector(".btnFive").textContent;
+
+
+let btnSix = document.querySelector(".btnSix");
+let sixValue = document.querySelector(".btnSix").textContent;
+
+
+let btnSeven = document.querySelector(".btnSeven");
+let sevenValue = document.querySelector(".btnSeven").textContent;
+
+
+let btnEight = document.querySelector(".btnEight");
+let eightValue = document.querySelector(".btnEight").textContent;
+
+
+let btnNine = document.querySelector(".btnNine");
+let nineValue = document.querySelector(".btnNine").textContent;
+
+
+let btnZero = document.querySelector(".btnZero");
+let zeroValue = document.querySelector(".btnZero").textContent;
+
+
+let btnPlus = document.querySelector(".btnPlus");
+let plusValue = document.querySelector(".btnPlus").textContent;
+
+
+let btnMinus = document.querySelector(".btnMinus");
+let minusValue = document.querySelector(".btnMinus").textContent;
+
+
+let btnMultiply = document.querySelector(".btnMultiply");
+let multiplyValue = document.querySelector(".btnMultiply").textContent;
+
+
+let btnDivide = document.querySelector(".btnDivide");
+let divideValue = document.querySelector(".btnDivide").textContent;
+
+
+let btnClearAll = document.querySelector(".btnClearAll");
+let clearAllValue = document.querySelector(".btnClearAll").textContent;
+
+
+
+
+
+// ONE
+function inputOne(){
+    inputDisplay.value += oneValue;
+};
+const clickOne = btnOne.addEventListener("click", inputOne);
+
+
+// TWO
+function inputTwo(){
+    inputDisplay.value += twoValue;
+};
+const clickTwo = btnTwo.addEventListener("click", inputTwo);
+
+
+// THREE
+function inputThree(){
+    inputDisplay.value += threeValue;
+};
+const clickThree = btnThree.addEventListener("click", inputThree);
+
+
+// FOUR
+function inputFour(){
+    inputDisplay.value += fourValue;
+};
+const clickFour = btnFour.addEventListener("click", inputFour);
+
+
+// FIVE
+function inputFive(){
+    inputDisplay.value += fiveValue;
+};
+const clickFive = btnFive.addEventListener("click", inputFive);
+
+
+// SIX
+function inputSix(){
+    inputDisplay.value += sixValue;
+};
+const clickSix = btnSix.addEventListener("click", inputSix);
+
+// SEVEN
+function inputSeven(){
+    inputDisplay.value += sevenValue;
+};
+const clickSeven = btnSeven.addEventListener("click", inputSeven);
+
+// EIGHT
+function inputEight(){
+    inputDisplay.value += eightValue;
+};
+const clickEight = btnEight.addEventListener("click", inputEight);
+
+// NINE
+function inputNine(){
+    inputDisplay.value += nineValue;
+};
+const clickNine = btnNine.addEventListener("click", inputNine);
+
+// ZERO
+function inputZero(){
+    inputDisplay.value += zeroValue;
+};
+const clickZero = btnZero.addEventListener("click", inputZero);
+
+
+// PLUS
+function inputPlus(){
+    inputDisplay.value += plusValue;
+};
+const clickPlus = btnPlus.addEventListener("click", inputPlus);
+
+
+// MINUS
+function inputMinus(){
+    inputDisplay.value += minusValue;
+};
+const clickMinus = btnMinus.addEventListener("click", inputMinus);
+
+
+// MULTIPLY
+function inputMultiply(){
+    inputDisplay.value += multiplyValue;
+};
+const clickMultiply = btnMultiply.addEventListener("click", inputMultiply);
+
+
+// DIVIDE
+function inputDivide(){
+    inputDisplay.value += divideValue;
+};
+const clickDivide = btnDivide.addEventListener("click", inputDivide);
+
+
+// CLEAR ALL
+function inputClearAll(){
+    inputDisplay.value = "";
+};
+const clickClearAll = btnClearAll.addEventListener("click", inputClearAll);
+
+
+
